8bd9d41791a4fae6c81ce5d65b406951aee3efe3
BORRACH <eduardorodriguesp254@gmail.ccom>
2023-08-03 22:01:45 -0300

    initial database

---
+++ data/Login.php
@@ -0,0 +1,11 @@
+<?php
+
+    $conexao = mysqli_connect('localhost', 'root', '')
+        or die("Erro na conexão com o MySQL: " . mysqli_connect_error());
+
+    // ----------------------------------------------------------- //
+
+    
+ 
+
+?>
